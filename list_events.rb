3326bdf27de843f88d5a2e900692ef1149cfc8d6
Mark Tearle <mark@tearle.com>
2015-12-14 21:34:12 +0800

    Fix up list_events output and to list events for past day

--- list_events.rb
+++ list_events.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/env ruby 
+
 require 'rubygems'
 require 'bundler/setup'
 require 'nokogiri'
@@ -16,22 +18,16 @@ cli = Viewpoint::EWSClient.new endpoint, user, pass, server_version: SOAP::Excha
 
 # see https://github.com/WinRb/Viewpoint/issues/184
 
-
-start_date = (Time.now - 386400).to_datetime
+# events in past day
+start_date = (Time.now - 86400).to_datetime
 end_date = Time.now.to_datetime
 
-p start_date
-p end_date
-
-#calendar = cli.get_folder(:calendar)
-#items = calendar.items_between sd, ed
+puts "Events from:\n#{start_date}\nto\n#{end_date}\n\n"
 
 events = cli.find_items({:folder_id => :calendar, :calendar_view => {:start_date => start_date, :end_date => end_date}})
 
 events.each do |event|
-
-  p event.subject
-  puts "#{event.start} - #{event.end}\t #{event.subject}"
+  puts "#{event.start} - #{event.end}\n\t#{event.subject}"
 end
 
 # see http://answer.io/p/ddacunha/Viewpoint
